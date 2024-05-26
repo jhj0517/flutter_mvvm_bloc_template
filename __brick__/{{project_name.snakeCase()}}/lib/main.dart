4d49ec691d35149b5123894b1676e37752815732
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-05-26 23:15:44 +0900

    migrate to bloc

--- __brick__/{{project_name.snakeCase()}}/lib/main.dart
+++ __brick__/{{project_name.snakeCase()}}/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_mvvm_bloc_temp/presentation/blocs/theme/theme_bloc.dart';
+import 'package:flutter_mvvm_bloc_temp/presentation/blocs/theme/theme_state.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'generated/l10n.dart';
 import 'presentation/blocs/home/home_bloc.dart';
 import 'presentation/views/views.dart';
-import 'presentation/providers/providers.dart';
 import 'data/repositories/repositories.dart';
 import 'di/dependency_injection.dart';
 
@@ -24,30 +25,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => HomeBloc(
-              memoRepository: locator<MemoRepository>(),
-              binanceRepository: locator<BinanceRepository>()
+        providers: [
+          BlocProvider(
+            create: (context) => HomeBloc(
+                memoRepository: locator<MemoRepository>(),
+                binanceRepository: locator<BinanceRepository>()
+            ),
           ),
-        ),
-      ],
-      child: Consumer<ThemeProvider>(
-        builder: (context, themeProvider, child) {
-          return MaterialApp(
-            title: 'Flutter MVVM Provider Template',
-            localizationsDelegates: const [
-              S.delegate,
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            supportedLocales: S.delegate.supportedLocales,
-            theme: themeProvider.attrs.colors,
-            home: const MyHomePage(),
-          );
-        },
-      )
+          BlocProvider(
+            create: (context) => ThemeBloc(prefs: locator<SharedPreferences>()),
+          ),
+        ],
+        child: BlocBuilder<ThemeBloc, ThemeState>(
+          builder: (context, state) {
+            ThemeData themeData = ThemeData.light();
+            if (state is ThemeLoaded) {
+              themeData = state.attrs.colors;
+            }
+
+            return MaterialApp(
+              title: 'Flutter MVVM Bloc Template',
+              localizationsDelegates: const [
+                S.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              supportedLocales: S.delegate.supportedLocales,
+              theme: themeData,
+              home: const MyHomePage(),
+            );
+          },
+        )
     );
   }
 }
